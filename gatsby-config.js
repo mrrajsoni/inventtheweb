587341c23737074603c183612e8d359d582fac50
Raj Soni <rajsoni19619@gmail.com>
2022-01-31 15:50:10 +0530

    Added Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `A freelance Frontend Developer from Mumbai`,
-    description: `Superfast E-commerce development that generate the sales.`,
+    description: `A superfast E-commerce development that generate the sales.`,
     author: `Raj Soni`,
     siteUrl: `https://inventtheweb.com`,
     menuLinks: [
@@ -54,6 +54,31 @@ module.exports = {
       resolve: `gatsby-plugin-sass`,
       options: {
         implementation: require("sass"),
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-165863268-1", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
