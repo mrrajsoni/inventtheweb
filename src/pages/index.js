8b60e8a1bd34a5188909d430e176272509d06d48
Raj Soni <rajsoni19619@gmail.com>
2022-02-11 17:09:36 +0530

    Maintenance mode

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import * as React from "react"
 import "../styles/global.scss"
 import "normalize.css"
-import Header from "../components/Header/header"
+// import Header from "../components/Header/header"
+import Maintenance from "./maintenance"
 
 const IndexPage = () => (
   <>
-    <Header />
+    <Maintenance />
   </>
 )
 
