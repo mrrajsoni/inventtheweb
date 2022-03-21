78c98621ac48dd21907283148853a011401f634b
Raj Soni <rajsoni19619@gmail.com>
2022-03-21 10:04:47 +0530

    Add development icons homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,57 @@
 import * as React from "react"
-import "../styles/global.scss"
 import "../styles/index.scss"
 import Header from "../components/Header/header"
 import Button from "../components/Button"
 import CaseStudy from "../components/Case-Studies/Case-studies"
 import Footer from "../components/Footer/Footer"
-import "../scripts/scroll.js"
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => {
+  const DevelopmentLogos = () => {
+    return (
+      <>
+        <StaticImage
+          className="logo-development"
+          src="../images/WordPress-logo.png"
+          loading="eager"
+          alt="WordPress-logo"
+        />
+        <StaticImage
+          className="logo-development"
+          src="../images/Shopify-logo.png"
+          loading="eager"
+          alt="Shopify-logo"
+        />
+        <StaticImage
+          className="logo-development"
+          src="../images/Gatsby-logo.png"
+          loading="eager"
+          alt="Gatsby-logo"
+        />
+      </>
+    )
+  }
   return (
     <>
       <Header />
-      <section className={`homepage-banner cd-section visible`}>
-        <div
-          className={`homepage-banner-left flex flex-col justify-center items-start `}
-        >
+      <section className={`homepage-banner text-center cd-section visible`}>
+        <div className={`homepage-banner-left flex flex-col items-center `}>
           <h1
-            title="Superfast E-commerce Website Development"
+            title="E-commerce Website Developer in Mumbai"
             className={`homepage-banner-title`}
           >
-            Superfast <br />
-            E-commerce Website Development
+            E-commerce Website Developer in Mumbai
           </h1>
           <p className={`homepage-banner-subtitle`}>
-            WordPress, Shopify e-commerce website that generate sales with
-            conversion optimized development.
+            WordPress, Shopify e-commerce website development that keenly
+            focuses on blazing-fast sites which in turn is responsible for
+            generating sales.
           </p>
-          <Button link="mailto:raj@inventtheweb.com" name={`Say hi 👋`} />
+          <Button link="mailto:raj@inventtheweb.com" name={`Hire me`} />
         </div>
-        <div className={`homepage-banner-right`}></div>
+        <div className={`homepage-banner-right flex mt-10 `}>
+          <DevelopmentLogos />
+        </div>
       </section>
       <section className={`case-study cd-section`}>
         <div className={`margin-container`}>
